415a15dd9b376790b403b10bccc18e055847af35
Rofael Aleezada <me@rofael.com>
2018-09-18 21:36:47 -0500

    Added algorithm code

--- assn2/q14_binary_ternary_search.py
+++ assn2/q14_binary_ternary_search.py
@@ -1,9 +1,53 @@
 import os
 
+# Binary search - log2(n)
+# Ternary search - log3(n) = log2(n)/log2(3) ~ 0.637log2(n) -> log2(n)
 
-def binary_search(array, val):
+
+def binary_search(arr, val, a, b):
+    if a > b or (a == b and arr[a] != val):
+        return False
+
+    mid = int((a + b) / 2)
+    mid_val = arr[mid]
+
+    if mid_val == val:
+        return True
+
+    if mid_val > val:
+        return binary_search(arr, val, a, mid - 1)
+
+    return binary_search(arr, val, mid + 1, b)
+
     pass
 
 
-def ternary_search(array, val):
+def ternary_search(arr, val, a, b):
+
+    if a > b or (a == b and arr[a] != val):
+        return False
+
+    gap = int((b - a + 1) / 3)
+    first_mid = a + gap
+    first_mid_val = arr[first_mid]
+    second_mid = first_mid + gap
+    second_mid_val = arr[second_mid]
+
+    if first_mid_val == val or second_mid_val == val:
+        return True
+
+    if val < first_mid_val:
+        return ternary_search(arr, val, a, first_mid - 1)
+
+    if val > first_mid_val and val < second_mid_val:
+        return ternary_search(arr, val, first_mid + 1, second_mid - 1)
+
+    return ternary_search(arr, val, second_mid + 1, b)
+
     pass
+
+
+if __name__ == "__main__":
+    arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+
+    print(ternary_search(arr, 16, 0, len(arr)-1))
--- assn2/q15_bruteforce_and_d_and_c.py
+++ assn2/q15_bruteforce_and_d_and_c.py
@@ -1,9 +1,89 @@
 import os
 
 
-def bruteforce_find_inversions(arr):
-    pass
+def brute_force_find_inversions(arr):
+    count = 0
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            # i is always less than j
+            if arr[i] > arr[j]:
+                count += 1
+
+    return count
+
+
+def _merge(arr, a, m, b):
+    left_half_pos = a
+    right_half_pos = m+1
+    copy = (b - a + 1) * [0]
+    cur_pos = 0
+    inversions = 0
+
+    while left_half_pos <= m and right_half_pos <= b:
+        if arr[left_half_pos] <= arr[right_half_pos]:
+            copy[cur_pos] = arr[left_half_pos]
+            left_half_pos += 1
+        else:
+            copy[cur_pos] = arr[right_half_pos]
+            right_half_pos += 1
+
+            # The number of inversions is equal to the amount of values from
+            # arr[left_hand_pos] to arr[m]
+            inversions += m - left_half_pos + 1
+        cur_pos += 1
+
+    while left_half_pos <= m:
+        copy[cur_pos] = arr[left_half_pos]
+        left_half_pos += 1
+        cur_pos += 1
+
+    while right_half_pos <= b:
+        copy[cur_pos] = arr[right_half_pos]
+        right_half_pos += 1
+        cur_pos += 1
+
+    for i in range(a, b + 1):
+        arr[i] = copy[i - a]
+
+    return inversions
+
+
+def _merge_sort(arr, a, b):
+    inversions = 0
+    if a < b:
+        mid = int((a + b) / 2)
+        inversions += _merge_sort(arr, a, mid)
+        inversions += _merge_sort(arr, mid + 1, b)
+        inversions += _merge(arr, a, mid, b)
+
+    return inversions
+
+
+def _binary_search(arr, val, a, b):
+    if a > b:
+        return -1
+
+    if a == b and arr[a] == val:
+        return a
+
+    mid = int((a + b) / 2)
+    mid_val = arr[mid]
+
+    if mid_val == val:
+        return mid
+
+    if mid_val > val:
+        return _binary_search(arr, val, a, mid - 1)
+
+    return _binary_search(arr, val, mid + 1, b)
 
 
 def divide_conquer_find_inversions(arr):
-    pass
+    return _merge_sort(arr, 0, len(arr)-1)
+
+
+
+if __name__ == "__main__":
+    arr = [9,5,2,4,3,6,0]
+    print(brute_force_find_inversions(arr))
+    print(divide_conquer_find_inversions(arr))
